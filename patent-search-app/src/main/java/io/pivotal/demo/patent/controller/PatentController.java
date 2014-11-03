89f8ccbfb2853c48b08165f1b9d4e77fbbbf8e90
Alberto A. Flores <alflores@gopivotal.com>
2014-11-03 09:42:07 -0500

    Added environment screen info

--- patent-search-app/src/main/java/io/pivotal/demo/patent/controller/PatentController.java
+++ patent-search-app/src/main/java/io/pivotal/demo/patent/controller/PatentController.java
@@ -1,5 +1,12 @@
 package io.pivotal.demo.patent.controller;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import io.pivotal.demo.patent.internal.Patent;
 import io.pivotal.demo.patent.internal.SearchCriteria;
 
@@ -15,6 +22,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Controller
 @RequestMapping("/")
 public class PatentController {
@@ -22,8 +31,9 @@ public class PatentController {
 	@Autowired private io.pivotal.demo.patent.internal.PatentRepository patentRepository;
 	
 	@RequestMapping
-	public ModelAndView main() {
-		return new ModelAndView("patents/main");
+	public String main(Model model) throws Exception {
+		updateModel(model);
+		return "patents/main";
 	}
 	
 	@RequestMapping(value = "patents/search", method = RequestMethod.GET)
@@ -57,5 +67,40 @@ public class PatentController {
 	public void kill() {
 		logger.warn("*** The system is shutting down. ***");
 		System.exit(-1);
+	}
+	
+	private void updateModel(Model model) throws Exception {
+		Date date = new Date();
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z");
+		String serverTime = dateFormat.format(date);
+		model.addAttribute("serverTime", serverTime);
+		
+		String port = System.getenv("PORT");
+		model.addAttribute("port", port);
+
+		String vcapApplication = System.getenv("VCAP_APPLICATION");
+		
+		if (vcapApplication != null) {
+			ObjectMapper mapper = new ObjectMapper();
+			Map vcapMap = mapper.readValue(vcapApplication, Map.class);
+			model.addAttribute("vcapApplication", vcapMap);
+		} else {
+			// VCAP NOT SET (not a CF deployment)
+			Map vcapMap = new HashMap<String, String>();
+			vcapMap.put("started_at", new Date()); // so that UI layer doesn't blow
+			model.addAttribute("vcapApplication", vcapMap);
+		}
+		
+		String vcapServices = System.getenv("VCAP_SERVICES");
+		if (vcapServices != null) {
+			ObjectMapper mapper = new ObjectMapper();
+			Map vcapMap = mapper.readValue(vcapServices, Map.class);
+			model.addAttribute("vcapServices", vcapMap.keySet());
+		} else {
+			// VCAP NOT SET (not a CF deployment)
+			model.addAttribute("vcapServices", new HashMap<String, String>());
+		}
+		
+		logger.info("Current date and time = [{}], port = [{}].", serverTime, port);
 	}
 }
